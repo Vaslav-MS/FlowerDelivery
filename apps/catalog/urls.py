139e5e95b5fbb1d5aa8f5618cdd93d021cd5416a
Vaslav <2key@rambler.ru>
2025-02-19 17:10:22 +0500

    edit catalog

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import product_list, product_detail
+from .views import product_list
 
 app_name = "catalog"
 
 urlpatterns = [
     path("", product_list, name="list"),
-    path("<int:product_id>/", product_detail, name="detail"),
 ]
 
