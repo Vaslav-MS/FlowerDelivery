da035a15e253ae1c268028cec1224e89f6333f23
Vaslav <2key@rambler.ru>
2025-02-19 17:37:16 +0500

    catalog corr

--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 
 class Category(models.Model):
@@ -16,7 +17,19 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
     description = models.TextField(blank=True, verbose_name="Описание")
     image = models.ImageField(upload_to="products/", blank=True, null=True, verbose_name="Изображение")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата добавления")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")  # Добавлено
+
+    def save(self, *args, **kwargs):
+        """Удаляет старое изображение при загрузке нового"""
+        try:
+            old_instance = Product.objects.get(id=self.id)
+            if old_instance.image and old_instance.image != self.image:
+                if os.path.isfile(old_instance.image.path):
+                    os.remove(old_instance.image.path)
+        except Product.DoesNotExist:
+            pass
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
